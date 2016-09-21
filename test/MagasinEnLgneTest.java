3110632a72d65a778cf2e7e967a9de7f0c330c90
fantay <laurentcoll@hotmail.com>
2016-09-21 14:59:15 +0200

    MagasinEnLgneTest.java
    ajout d'une 2 categorie et d'un produit de cette categorie

--- test/MagasinEnLgneTest.java
+++ test/MagasinEnLgneTest.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import magasinenligne.entity.Categorie;
+import magasinenligne.entity.Produit;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,25 +17,34 @@ import static org.junit.Assert.*;
  * @author Laurent-LIM
  */
 public class MagasinEnLgneTest {
-    
+
     @Test
-    public void test(){
-        
+    public void test() {
+
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
-        
+
         // permet d'ouvrir la manipulation des donnees en base
         em.getTransaction().begin();
-        
+
         //ecriture dans categorie
         Categorie c1 = new Categorie();
-        c1.setNom("Chaussure a talon");
-        
+        c1.setNom("Basket");
+
         //ajout dans la base
         em.persist(c1);
-        
+
+        /* ajout 2 eme categorie*/
+        Categorie c2 = new Categorie();
+        c2.setNom("Lunettes solaire");
+        em.persist(c2);
+
+        Produit rayban = new Produit();
+        rayban.setCategorie(c2);
+        em.persist(rayban);
+
         //permet d'envoyer la transaction a la base
         em.getTransaction().commit();
-        
+
     }
-    
+
 }
