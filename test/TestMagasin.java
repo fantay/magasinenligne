830b18da6c75534f203a62f5138a684cb27b03c0
fantay <laurentcoll@hotmail.com>
2016-09-22 10:35:15 +0200

    TestMagasin.java
    ajout de @Before
    - vide toutes les tables
    - Ajout de données afin de faire no test
    
    ajout du test de comparaison d'une categorie

--- test/TestMagasin.java
+++ test/TestMagasin.java
@@ -7,79 +7,108 @@
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import magasinenligne.entity.Categorie;
 import magasinenligne.entity.Client;
 import magasinenligne.entity.Produit;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author Laurent-LIM
  */
 public class TestMagasin {
-
-    @Test
-    public void testAjoutClient() {
-
+    
+    @Before
+    public void avant() {
+        
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
-
         em.getTransaction().begin();
+        
+        /* On vide toutes les tables */
+        //methode 1
+        Query query = em.createQuery("DELETE FROM Produit p");
+        query.executeUpdate();
+        
+        //methode 2
+        em.createQuery("DELETE FROM Categorie p").executeUpdate();
 
-        //ajout de client
-        Client c1 = new Client();
-        c1.setLogin("toto");
-        em.persist(c1);
+        /*  
+            ajoute des données en spécifiant les ID que 
+            l'on va utliser dans les test unitaires 
+        */
 
-        Client c2 = new Client();
-        c2.setLogin("tata");
-        em.persist(c2);
-
-        em.getTransaction().commit();
-
-    }
-
-    //@Test
-    public void testListeProdCategorie() {
-
-        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
-
-        Categorie cat = em.find(Categorie.class, 2L);
-        for (Produit p : cat.getProduits()) {
-
-            System.out.println(p);
-        }
-
-    }
-
-    // @Test
-    public void test() {
-
-        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
-
-        // permet d'ouvrir la manipulation des donnees en base
-        em.getTransaction().begin();
-
-        //ecriture dans categorie
+        //ajout d'unr categorie
         Categorie c1 = new Categorie();
+        c1.setId(1L);
         c1.setNom("Basket");
-
-        //ajout dans la base
         em.persist(c1);
 
         /* ajout 2 eme categorie*/
         Categorie c2 = new Categorie();
+        c2.setId(2L);
         c2.setNom("Lunettes solaire");
         em.persist(c2);
 
+        /*ajout d'un produit avec reference de sa categorie*/
         Produit rayban = new Produit();
+        rayban.setId(1L);
         rayban.setTitre("ray-Ban");
         rayban.setCategorie(c2);
         em.persist(rayban);
 
         //permet d'envoyer la transaction a la base
         em.getTransaction().commit();
-
+        
     }
 
+    /* ############################ TEST ################################## */
+    @Test
+    public void verifieQueCatID1EstBasket() {
+        
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        
+        Categorie cat = em.find(Categorie.class, 1L);
+        
+        if (cat.getNom().equals("Basket")==false) {
+           Assert.fail("CA MARCHE PAS");
+        }
+    }
+
+    //@Test
+    public void testAjoutClient() {
+        
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        
+        em.getTransaction().begin();
+
+        //ajout de client
+        Client c1 = new Client();
+        c1.setLogin("toto");
+        em.persist(c1);
+        
+        Client c2 = new Client();
+        c2.setLogin("tata");
+        em.persist(c2);
+        
+        em.getTransaction().commit();
+        
+    }
+
+    //@Test
+    public void testListeProdCategorie() {
+        
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        
+        Categorie cat = em.find(Categorie.class, 1L);
+        for (Produit p : cat.getProduits()) {
+            
+            System.out.println(p);
+        }
+        
+    }
+    
 }
